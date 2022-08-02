2dfed06f2d69b276111fee6f1ecd02f0ab795500
abhinav190202 <abhinav19022002@gmail.com>
2022-08-02 12:07:38 +0530

    Port reconfigured

--- index.js
+++ index.js
@@ -100,5 +100,5 @@ app.use('/internships', internshipRoute);
 app.use('/', dashboardRoute);
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => { console.log(`server is running on : ${PORT}`) })
